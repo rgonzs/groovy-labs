ec8dff20be837da29852ad5e50d048997b554170
rgonzs <48256361+rgonzs@users.noreply.github.com>
2025-06-09 04:02:29 -0500

    fix 3

--- vars/scanContainerImage.groovy
+++ vars/scanContainerImage.groovy
@@ -8,8 +8,8 @@ def call(String image) {
     String name = actualVersion.name
     sh script: """
     curl -LO $downloadUrl
-    mkdir -p "$(pwd)/trivy"
-    tar -xzf ${actualVersion.name} -C "$(pwd)/trivy"
+    mkdir -p $(pwd)/trivy
+    tar -xzf ${actualVersion.name} -C $(pwd)/trivy
     """, label: 'Download trivy'
     sh script: "./trivy/trivy -q image --input ${image} --cache-dir /tmp/trivy-cache", label: 'Scanning image'
 }
